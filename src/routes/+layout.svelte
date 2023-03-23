1d57549abb1af29f5dc3ce3856f90ae87a2e464c
KekOnTheWorld <kekontheworld@gmail.com>
2023-03-23 20:13:08 +0100

    Add loading indicator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import '@fontsource/fira-mono/latin-400.css';
+
+	import PreloadingIndicator from "$lib/PreloadingIndicator.svelte";
+	import { navigating } from "$app/stores";
 </script>
+
+{#if $navigating && $navigating.to}
+	<PreloadingIndicator />
+{/if}
 
 <slot />
