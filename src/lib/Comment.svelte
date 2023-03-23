5099a63b23549200005b726dbe488caa5d2a92f8
KekOnTheWorld <kekontheworld@gmail.com>
2023-03-23 19:49:59 +0100

    Fix comment border

--- src/lib/Comment.svelte
+++ src/lib/Comment.svelte
@@ -78,6 +78,7 @@
 		display: flex;
 		align-items: center;
 		border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+		border-radius: 5px 5px 0 0;
 		background-color: #111;
 		padding: 10px;
 		font-size: 14px;
