f0a1d57fa71c069d20eac3fd2b645fe33b966f8c
KekOnTheWorld <kekontheworld@gmail.com>
2023-04-20 17:43:27 +0200

    ToDo -> TODO

--- src/routes/(protected)/(project)/scrum/+page@(protected).svelte
+++ src/routes/(protected)/(project)/scrum/+page@(protected).svelte
@@ -11,7 +11,7 @@
 	let columns = [
 		{ name: 'Story', handle: (v) => v },
 		{
-			name: 'ToDo',
+			name: 'TODO',
 			async handle(v) {
 				if (v.task) {
 					await fetch(`/tasks/${v.task.id}?/unassign`, {
